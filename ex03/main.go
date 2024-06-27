ddc399542f567ea55c1e6ee4cde24d4adf575f48
tsukasakop <tsukasakop@gmail.com>
2024-06-27 19:38:42 +0900

    ✨ Implement ex03

---
+++ ex03/main.go
@@ -0,0 +1,12 @@
+package main
+import (
+        "fmt"
+        "piscine"
+)
+func main() {
+        a := 13
+b := 2
+piscine.UltimateDivMod(&a, &b)
+fmt.Println(a)
+fmt.Println(b)
+}
---
+++ ex03/vendor/piscine/ultimatedivmod.go
@@ -0,0 +1,6 @@
+package piscine
+func UltimateDivMod(a *int, b *int) {
+tmp:=*a
+*a=tmp / *b
+*b=tmp%*b
+}
