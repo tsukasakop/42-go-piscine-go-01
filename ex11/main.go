712ad7f3745a0926d277188c0309e6f20f2aad04
tsukasakop <tsukasakop@gmail.com>
2024-06-29 19:50:02 +0900

    ✨ Implement ex11

---
+++ ex11/main.go
@@ -0,0 +1,10 @@
+package main
+import (
+        "fmt"
+        "piscine"
+)
+func main() {
+s := []int{5,4,3,2,1,0}
+piscine.SortIntegerTable(s)
+fmt.Println(s)
+}
---
+++ ex11/vendor/piscine/sortintegertable.go
@@ -0,0 +1,15 @@
+package piscine
+
+func SortIntegerTable(table []int) {
+	l := 0
+	for range table {
+		l++
+	}
+	for i:=0; i < l-1; i++ {
+		for j:=0; j < l-1-i; j++ {
+			if table[j] > table[j+1] {
+				table[j], table[j+1] = table[j+1], table[j]
+			}
+		}
+	}
+}
